1101f5e041c99631d11da86488377934560c62a1
salapao2136 <potook_02@hotmail.com>
2016-04-30 22:18:37 +0700

    edit bug

--- index.js
+++ index.js
@@ -2,22 +2,16 @@ var express = require('express')
 var app = express()
 var body = require('body-parser')
 var json = body.json()
-
+var request = require('request')
 app.use(express.static('public'))
 
 app.post('/search', json, function (req, res) {
-  var send
-  var get = apis(send, req.body)
-  // console.log(get)
-  res.send('reqs')
+  var data = req.body
+  request('https://maps.googleapis.com/maps/api/place/textsearch/json?location=' + data.location_now + '&query=' + data.test + '&radius=' + data.radius + '&key=AIzaSyDTx6k0EMtaMUJL1gP9w4rDc4qwp8LnDMc', function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      res.send(body.results)
+    }
+  })
 })
 
-function apis (send, data) {
-  // console.log('https://maps.googleapis.com/maps/api/place/textsearch/json?location=' + data.location_now + '&query=' + data.test + '&radius=' + data.radius + '&key=AIzaSyDTx6k0EMtaMUJL1gP9w4rDc4qwp8LnDMc');
-  app.get('https://maps.googleapis.com/maps/api/place/textsearch/json?location=' + data.location_now + '&query=' + data.test + '&radius=' + data.radius + '&key=AIzaSyDTx6k0EMtaMUJL1gP9w4rDc4qwp8LnDMc', function (req, res) {
-    console.log('gg');
-    send = reqs
-  })
-  return send
-}
 app.listen(3000, function () {})
