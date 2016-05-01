ca147a9b8358488dcfe0321d5d54931e1561c0e4
salapao2136 <potook_02@hotmail.com>
2016-05-01 19:38:39 +0700

    distance

--- index.js
+++ index.js
@@ -7,7 +7,16 @@ app.use(express.static('public'))
 
 app.post('/search', json, function (req, res) {
   var data = req.body
-  request('https://maps.googleapis.com/maps/api/place/textsearch/json?location=' + data.location_now + '&query=' + data.test + '&radius=' + data.radius + '&key=AIzaSyDTx6k0EMtaMUJL1gP9w4rDc4qwp8LnDMc', function (error, response, body) {
+  request('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + data.location_now + '&query=' + data.test + '&radius=' + data.radius + '&key=AIzaSyA9pnLokpiT4egOd3J5Lhfb1I5PHmwyyXk', function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      res.send(body)
+    // console.log(body)
+    }
+  })
+})
+app.post('/ways', json, function (req, res) {
+  var datas = req.body
+  request('https://maps.googleapis.com/maps/api/directions/json?origin=14.16631,101.3544531&destination=14.1644697,101.3635071&key=AIzaSyA9pnLokpiT4egOd3J5Lhfb1I5PHmwyyXk', function (error, response, bodys) {
     if (!error && response.statusCode === 200) {
       res.send(body)
     }
--- public/app.js
+++ public/app.js
@@ -5,20 +5,36 @@ angular.module('todoApp', ['ui.materialize'])
     $scope.radius = 500
     $scope.location_now = '13.7468351,100.5327397'
     $scope.select = function () {
+      var data = {
+        location_now: $scope.location_now,
+        test: $scope.test,
+        radius: $scope.radius
+      }
       $scope.load = true
       if ($scope.test !== '' && $scope.radius !== 0) {
-        var data = {
-          location_now: $scope.location_now,
-          test: $scope.test,
-          radius: $scope.radius
-        }
         $http.post('/search', data).then(function (req, res) {
-          $scope.load = false
-          $scope.map = req.data.results
-          console.log(req.data.results)
+          if (req.data.status === 'OK') {
+            $scope.load = false
+            $scope.map = req.data.results
+            $scope.ways()
+          }else if (req.data.status === 'OVER_QUERY_LIMIT') {
+            $scope.load = false
+            alert('time LIMIT')
+          }
+        console.log(req.data.results)
         })
       } else {
         $scope.load = false
       }
+    }
+    $scope.ways = function () {
+      var data = {
+        location_now: $scope.location_now,
+        test: $scope.test,
+        radius: $scope.radius
+      }
+      $http.post('/ways', data).then(function (req, res) {
+        console.log(req.data)
+      })
     }
   })
