a392abd84ea16b9f26beb7c5cf2fd5ed96c674b1
salapao2136 <potook_02@hotmail.com>
2016-05-08 12:57:07 +0700

    final

--- public/app.js
+++ public/app.js
@@ -2,7 +2,7 @@
 angular.module('todoApp', ['ui.materialize'])
   .controller('TodoListController', function ($http, $scope) {
     $scope.test = ''
-    $scope.radius = 500
+    $scope.radius = 10
     $scope.routes = []
     $scope.location_now = '13.7468351,100.5327397'
     $scope.key = 'AIzaSyA9pnLokpiT4egOd3J5Lhfb1I5PHmwyyXk'
@@ -61,27 +61,28 @@ angular.module('todoApp', ['ui.materialize'])
           } else {
             pho = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=' + item.photos[0].photo_reference + '&key=' + $scope.key
           }
-
-          var temp = {
-            icon: item.icon,
-            name: item.name,
-            geometry: {
-              location: {
-                lat: item.geometry.location.lat,
-                lng: item.geometry.location.lng
-              }
-            },
-            distance: req.data.routes[0].legs[0].distance,
-            duration: req.data.routes[0].legs[0].duration,
-            photos: pho,
-            vicinity: item.formatted_address
+          if (req.data.routes[0].legs[0].distance.value <= ($scope.radius * 1000)) {
+            var temp = {
+              icon: item.icon,
+              name: item.name,
+              geometry: {
+                location: {
+                  lat: item.geometry.location.lat,
+                  lng: item.geometry.location.lng
+                }
+              },
+              distance: req.data.routes[0].legs[0].distance,
+              duration: req.data.routes[0].legs[0].duration,
+              photos: pho,
+              vicinity: item.formatted_address
+            }
+            $scope.routes.push(temp)
+            $scope.routes.sort(function (a, b) { // เรียงค่า น้อย ไป มาก
+              if (a.distance.value > b.distance.value) return 1
+              if (a.distance.value < b.distance.value) return -1
+              return 0
+            })
           }
-          $scope.routes.push(temp)
-          $scope.routes.sort(function (a, b) { // เรียงค่า น้อย ไป มาก
-            if (a.distance.value > b.distance.value) return 1
-            if (a.distance.value < b.distance.value) return -1
-            return 0
-          })
         // console.log($scope.routes)
         } else if (req.data.status === 'OVER_QUERY_LIMIT') {
           console.log('Time LIMIT(ระยะทาง)')
